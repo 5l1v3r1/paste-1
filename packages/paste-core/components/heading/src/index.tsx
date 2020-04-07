a6455b94670529f14c69846ab3cbd47fd275e519
Si Taggart <me@simontaggart.com>
2020-04-06 19:01:49 -0700

    fix(heading): type the heading to a heading element

--- packages/paste-core/components/heading/src/index.tsx
+++ packages/paste-core/components/heading/src/index.tsx
@@ -5,7 +5,7 @@ import {Text, safelySpreadTextProps} from '@twilio-paste/text';
 export type asTags = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'div' | 'label' | 'span' | 'header';
 export type HeadingVariants = 'heading10' | 'heading20' | 'heading30' | 'heading40' | 'heading50' | 'heading60';
 
-export interface HeadingProps extends React.AllHTMLAttributes<HTMLHeadingElement> {
+export interface HeadingProps extends React.HTMLAttributes<HTMLHeadingElement> {
   as: asTags;
   className?: never;
   id?: string;
